8e21b354e50247ce81435199541d92384de6892a
MihaOnTech <mihaontech@gmail.com>
2023-04-13 11:40:47 +0200

    Initial Commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#2c2c2c",
+    },
+    text: {
+      primary: "#f0f0f0",
+    },
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <SessionProvider session={pageProps.session}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </SessionProvider>
+  );
 }
+
+export default MyApp;
